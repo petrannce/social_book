1e3b43de329e4dc6a82cf84136615521fe847dae
Peter Ndirangu <pndirangu788@gmail.com>
2024-11-22 22:21:07 +0300

    commit

--- core/views.py
+++ core/views.py
@@ -16,6 +16,27 @@ def index(request):
 
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('profileimg') == None:
+            image_file = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image_file
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('profileimg') != None:
+            image_file = request.FILES.get('profileimg')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image_file
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
     return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
